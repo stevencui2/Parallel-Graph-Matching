7f1ea11f9f5c120609d4418d5e1ba6aa187d675d
Hugo De Moraes <demoraeshugo@gmail.com>
2021-04-25 13:20:50 -0400

    Removed helper

--- src/gpu_required/strongestNeighborScan.cu
+++ src/gpu_required/strongestNeighborScan.cu
@@ -7,32 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__host__ int getStrongerThreadIndex(int * oldWeight, int * oldDst, int curTID, int compareTID) {
-    int strongerIndex;
-    const int COMPARE_T_WEIGHT = oldWeight[compareTID];
-    const int CUR_T_WEIGHT = oldWeight[curTID];
-
-    if(COMPARE_T_WEIGHT > CUR_T_WEIGHT) {
-        strongerIndex = compareTID;
-    }
-    else if(COMPARE_T_WEIGHT < CUR_T_WEIGHT) {
-        strongerIndex = curTID;
-    }
-    // case: equal weights, take node with smaller vID
-    else {
-        const int COMPARE_T_D = oldDst[compareTID];
-        const int CUR_T_D = oldDst[curTID];
-
-        if(COMPARE_T_D < CUR_T_D) {
-            strongerIndex = compareTID;
-        } else {
-            strongerIndex = curTID;
-        };
-    }
-
-    return strongerIndex;
-}
-
 __global__ void strongestNeighborScan_gpu(
         int * src,
         int * oldDst, int * newDst,
@@ -41,10 +15,8 @@ __global__ void strongestNeighborScan_gpu(
         int distance,
         int numEdges
         ) {
-    // Calculate thread work
+    // Calculate number of threads
     const int NUM_THREADS = blockDim.x * gridDim.x;
-    // const int THREAD_WORK = numEdges / NUM_THREADS;
-
     //Get thread ID
     const int FIRST_T_ID = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -54,12 +26,33 @@ __global__ void strongestNeighborScan_gpu(
 
         // case : shared segment
         if( src[COMPARE_T_ID] == src[curTID]) {
-            const int STRONGER_INDEX = getStrongerThreadIndex(oldWeight, oldDst, curTID, COMPARE_T_ID);
+            int strongerIndex;
+            const int COMPARE_T_WEIGHT = oldWeight[COMPARE_T_ID];
+            const int CUR_T_WEIGHT = oldWeight[curTID];
+
+            if(COMPARE_T_WEIGHT > CUR_T_WEIGHT) {
+                strongerIndex = COMPARE_T_ID;
+            }
+            else if(COMPARE_T_WEIGHT < CUR_T_WEIGHT) {
+                strongerIndex = curTID;
+            }
+            // case: equal weights, take node with smaller vID
+            else {
+                const int COMPARE_T_D = oldDst[COMPARE_T_ID];
+                const int CUR_T_D = oldDst[curTID];
+
+                if(COMPARE_T_D < CUR_T_D) {
+                    strongerIndex = COMPARE_T_ID;
+                } else {
+                    strongerIndex = curTID;
+                };
+            }
+
             //Set new destination
-            newDst[curTID] = oldDst[STRONGER_INDEX];
+            newDst[curTID] = oldDst[strongerIndex];
 
             //Set new weight
-            newWeight[curTID] = oldWeight[STRONGER_INDEX];
+            newWeight[curTID] = oldWeight[strongerIndex];
 
             //Flag any changes
             if(newDst[curTID] != oldDst[curTID]) { *madeChanges = 1; };
